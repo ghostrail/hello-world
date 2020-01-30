7dff1f63118afaed9322b6dba5d9a8537d5c0315
ghostrail <35690784+ghostrail@users.noreply.github.com>
2020-01-30 10:01:11 +0800

    Update babyname analysis.py

--- babyname analysis.py
+++ babyname analysis.py
@@ -1,5 +1,10 @@
+#import the namelist file
+file_path='D:/babyname list/yob1880.txt'
+file_context=open(file_path,'r')#以只读形式打开文件
+raw_data=file_context.readlines()
+#以字典形式逐行读取信息
+babyname=[]
 linenumber=0
-import linecache
 for line in raw_data:
     #split()函数分割单行
     dict={}
