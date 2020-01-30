49d9531d1a88c5702b3ab35470e59b4c3e890306
ghostrail <35690784+ghostrail@users.noreply.github.com>
2020-01-30 09:58:59 +0800

    Update babyname analysis.py

--- babyname analysis.py
+++ babyname analysis.py
@@ -1,22 +1,25 @@
-#import the namelist file
-file_path='D:/babyname list/yob1880.txt'
-file_context=open(file_path,'r')#以只读形式打开文件
-raw_data=file_context.readlines()
-#以字典形式逐行读取信息
-babyname=[]
-dict={'name':'Allen','gender':'M','number':100}
 linenumber=0
 import linecache
 for line in raw_data:
-    linenumber = linenumber + 1
-    #linecache输出特定行
-    namelist=linecache.getline(file_path,linenumber).strip()
-    #solit()函数分割单行
-    dict['name']=namelist.split(',')[0]
-    dict['gender']=namelist.split(',')[1]
-    dict['number']=int(namelist.split(',')[2])
+    #split()函数分割单行
+    dict={}
     babyname.append(dict)
-print(babyname)
+    babyname[linenumber]['name']=line.split(',')[0]
+    babyname[linenumber]['gender']=line.split(',')[1]
+    babyname[linenumber]['number']=int(line.split(',')[2])
+    linenumber = linenumber + 1
 
-#file_context.close()
+'''
+def exchange(small,large):
+    temp=babyname[small]
+    babyname[small]=babyname[large]
+    babyname[large]=temp
+    return babyname[small],babyname[large]
+
+print(babyname[3],babyname[7])
+exchange(3,7)
+print(babyname[3],babyname[7])
+'''
+file_context.close()
+
 
